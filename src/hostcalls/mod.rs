04f2a85637264758c1b77682a486289fbbdd0daa
joeywang <joeywdz@gmail.com>
2022-11-28 11:27:53 +0800

    fix: hostcall network

--- src/hostcalls/mod.rs
+++ src/hostcalls/mod.rs
@@ -1,6 +1,5 @@
 extern "C" {
     pub fn hostcall_set_outputs(outputs_ptr: *const u8, outputs_len: usize);
-    pub fn hostcall_request_inputs(inputs_ptr: *const u8, inputs_len: usize);
-    pub fn hostcall_response_outputs(outputs_ptr: *const u8, outputs_len: usize);
+    pub fn hostcall_request_inputs(inputs_ptr: *const u8, inputs_len: usize, new_ptr: usize) -> usize;
 }
  
